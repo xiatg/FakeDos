d99945f33e275e93957c223c5e24c06ca4c576e7
Tongguang Watanabe Xia <ghbq674521@163.com>
2020-05-26 12:33:50 +0800

    add delete user/change_user/ls_u/ls_lu

--- userManagement.cpp
+++ userManagement.cpp
@@ -10,9 +10,10 @@
 #include <userManagement.h>
 using namespace std;
 
+//缺少对用户所在目录的管制
 void create_user(vector<string> command_splited, vector<string> & user_name, map<string, string> & user_password, string fakeDosFolderPath) {
     if (command_splited.size() < 3) {
-        cout << "Error: Please enter valid user name and password." << endl;
+        cout << "Error: Please enter a valid user name and password." << endl;
         cout << "\tcreate_user (user name) (password)" << endl;
     } else {
         string username = command_splited[1];
@@ -44,7 +45,58 @@ void create_user(vector<string> command_splited, vector<string> & user_name, map
     }
 }
 
-void log_in(vector<string> command_splited, vector<string> user_name, map<string, string> user_password, bool & is_logged_in, string & current_user, string & current_path) {
+// 缺少对进程的管制
+// 缺少对用户所在目录的管制
+void delete_user(vector<string> command_splited, vector<string> & user_name, map<string, string> & user_password, string fakeDosFolderPath, vector<string> & logged_in_users) {
+    if (command_splited.size() < 2) {
+        cout << "Error: Please enter a valid user name." << endl;
+        cout << "\tdelete_user (user name)" << endl;
+    } else {
+        string username = command_splited[1];
+
+        vector<string>::iterator it = find(user_name.begin(), user_name.end(), username);
+
+        if (it != user_name.end()) {
+
+            cout << "Are you sure you want to delete " << username << " ? Enter password to continue:" << endl;
+
+            string password;
+
+            getline(cin, password);
+
+            if (password == user_password[username]) {
+                user_name.erase(it);
+                user_password.erase(username);
+                system(("rmdir /s /Q " + fakeDosFolderPath + "\\users\\" + username).c_str());
+
+                vector<string>::iterator it2 = find(logged_in_users.begin(), logged_in_users.end(), username);
+
+                if (it2 != logged_in_users.end()) {
+                    logged_in_users.erase(it2);
+                }
+
+                cout << "Successfully deleted the user: " << username << endl;
+
+            }
+            else {
+                cout << "Error: incorrent password." << endl;
+            }
+        } else {
+            cout << "Error: user name " << username << " does not exist." << endl;
+        }
+    }
+}
+
+void change_user(bool & is_logged_in) {
+    if (!is_logged_in) {
+        cout << "Error: you are already in the user select panel." << endl;
+    } else {
+        is_logged_in = false;
+    }
+}
+
+//缺少对用户目录的控制
+void log_in(vector<string> command_splited, vector<string> user_name, map<string, string> user_password, bool & is_logged_in, string & current_user, string & current_path, vector<string> & logged_in_users) {
 
     if (is_logged_in) {
         cout << "Error: you have already logged in!" << endl;
@@ -63,20 +115,49 @@ void log_in(vector<string> command_splited, vector<string> user_name, map<string
                 if (user_password[username] == password) {
                     cout << "Welcome back, " << username << "!" << endl;
                     is_logged_in = true;
+
+                    vector<string>::iterator it_liu = find(logged_in_users.begin(), logged_in_users.end(), username);
+                    if (it_liu == logged_in_users.end()) {
+                        logged_in_users.push_back(username);
+                    }
+
                     current_user = username;
                     current_path = "users\\" + username;
                 } else {
                     cout << "Error: incorrent password." << endl;
                 }
             } else {
-                cout << "Error: incorrent user name." << endl;
+                cout << "Error: username " << username << " does not exist." << endl;
             }
         }
     }
 
 }
 
-void log_out(bool & is_logged_in) {
-    is_logged_in = false;
-    cout << "You have logged out." << endl;
+// 缺少对进程的管制
+void log_out(bool & is_logged_in, string current_user, vector<string> & logged_in_users) {
+    if (!is_logged_in) {
+        cout << "Error: you have not logged in yet." << endl;
+    } else {
+        is_logged_in = false;
+
+        vector<string>::iterator it_liu = find(logged_in_users.begin(), logged_in_users.end(), current_user);
+        logged_in_users.erase(it_liu);
+
+        cout << "You have logged out." << endl;
+    }
+}
+
+void ls_u(vector<string> user_name) {
+    cout << user_name.size() << " users exist." << endl;
+    for (int i = 0; i < int(user_name.size()); i++) {
+        cout << user_name[i] << endl;
+    }
+}
+
+void ls_lu(vector<string> logged_in_users) {
+    cout << logged_in_users.size() << " users have currently logged in." << endl;
+    for (int i = 0; i < int(logged_in_users.size()); i++) {
+        cout << logged_in_users[i] << endl;
+    }
 }
--- userManagement.h
+++ userManagement.h
@@ -10,8 +10,16 @@
  */
 void create_user(std::vector<std::string> command_splited, std::vector<std::string> & user_name, std::map<std::string, std::string> & user_password, std::string fakeDosFolderPath);
 
-void log_in(std::vector<std::string> command_splited, std::vector<std::string> user_name, std::map<std::string, std::string> user_password, bool & is_logged_in, std::string & current_user, std::string & current_path);
+void delete_user(std::vector<std::string> command_splited, std::vector<std::string> & user_name, std::map<std::string, std::string> & user_password, std::string fakeDosFolderPath, std::vector<std::string> & logged_in_users);
 
-void log_out(bool & is_logged_in);
+void change_user(bool & is_logged_in);
+
+void log_in(std::vector<std::string> command_splited, std::vector<std::string> user_name, std::map<std::string, std::string> user_password, bool & is_logged_in, std::string & current_user, std::string & current_path, std::vector<std::string> & logged_in_users);
+
+void log_out(bool & is_logged_in, std::string current_user, std::vector<std::string> & logged_in_users);
+
+void ls_u(std::vector<std::string> user_name);
+
+void ls_lu(std::vector<std::string> logged_in_users);
 
 #endif // USERMANAGEMENT_H
