fae522aa77549bfe1f4014560e1aa51701eba6e7
J3pHR3-Y <47342498+J3pHR3-Y@users.noreply.github.com>
2020-05-29 00:37:15 +0800

    Update mem.cpp

--- mem.cpp
+++ mem.cpp
@@ -11,46 +11,32 @@
 using namespace std;
 
 string current_user;
-int apcount;
 int mem_capacity = 0xFFFF;
 string jsonmem;
+vector<string> user_name;
 
 void init_mem(){
     Json::StyledStreamWriter writer;
-    Json::Value root, user, taskid;
+    Json::Value root, user, taskmem;
 
-    root["loginuser"] = user;
-    root["taskid"] = taskid;
+    root["user"] = user;
+    root["taskmem"] = taskmem;
 
     jsonmem = root.toStyledString(); 
 }
 
-void user_mem_alloc(string & current_user){
+void user_mem_alloc(string username){
     Json::Reader reader;
-    Json::Value root, user, taskid;
+    Json::Value root;
     
     if (reader.parse(jsonmem, root)){
-        if (!root["loginuser"].isMember(current_user)){
-            user[(char*)current_user.c_str()] = 0x1000;
-            root["loginuser"].append(user);
+        if (get_userid(username) != -1){
+            root["user"][username] = 0x1000;
         }
         else{
-            root["loginuser"][current_user] = 0x1000;  //  0x1000 represent OS memory usage
+            root["user"][username] = 0x1000;  //  0x1000 represent OS memory usage
         }
         
-        int index;
-        for (int i = 0; i < user.size(); i++){
-            if (root["loginuser"][i].isMember(current_user)){
-                index = i;
-            }
-        }
-        //  define taskid for loginuser
-        string id = to_string(index);
-        Json::Value task;
-        task["ap1"] = id + "1";
-        task["ap2"] = id + "2";
-        task["ap3"] = id + "3";
-        root["taskid"] = task;
     }
     jsonmem = root.toStyledString(); 
 }
@@ -63,16 +49,12 @@ void task_mem(int taskid, int taskmem){
 
     if (reader.parse(jsonmem, root)){
         if (!root.isMember(id)){
-            Json::Value task, mem;
-            mem["memusage"] = taskmem;
-            task.append(mem);
-            root[id] = task;
+            root["taskmem"][id] = taskmem;
         }
-        else
-        {
-            root[id][0]["memusage"] = root[id][0]["memusage"].asInt() + taskmem;
+        else {
+            root["taskmem"][id] = root["taskmem"][id].asInt() + taskmem;
         }
-        root["loginuser"][current_user] = root["loginuser"][current_user].asInt() + taskmem;
+        root["user"][current_user] = root["user"][current_user].asInt() + taskmem;
     }
     jsonmem = root.toStyledString(); 
 }
@@ -84,9 +66,9 @@ bool task_data_write(int taskid, string key, ValueType value){
 
     string id = to_string(taskid);
     int datamem = sizeof(value);
-    if (limit_check(current_user, datamem)) return false;
+    if (limit_check(datamem)) return false;
     if (reader.parse(jsonmem, root)){
-        root[id][0][key] = value;
+        root[id][key] = value;
         task_mem(taskid, datamem);
     }
     jsonmem = root.toStyledString(); 
@@ -101,32 +83,26 @@ ValueType task_data_read(int taskid, string key){
     string id = to_string(taskid);
 
     if (reader.parse(jsonmem, root)){
-        return root[id][0][key];
+        return root[id][key];
     }
 }
 
 
-
-void user_mem_free(string & current_user){
+void user_mem_free(string username){
     Json::Reader reader;
     Json::Value root, user;
     
     if (reader.parse(jsonmem, root)){
-        int index;
-        for (int i = 0; i < root["loginuser"].size(); i++){
-            if (root["loginuser"][i].isMember(current_user)){
-                index = i;
-                root["loginuser"][i][current_user] = 0;
-            }
-        }
-
-        string id = to_string(index);
-        for (int i = 1; i < apcount; i++)
-            if (root.isMember(id + to_string(i))){
-                root.removeMember(id + to_string(i));
-            }
+        root["user"][username] = 0;
     }
     jsonmem = root.toStyledString();
+    ///////////////////////////////////////////////
+    // need to kill all task memory under this user
+    ///////////////////////////////////////////////
+
+
+
+    
 }
 
 void task_mem_free(int taskid){
@@ -135,23 +111,22 @@ void task_mem_free(int taskid){
     string id = to_string(taskid);
 
     if (reader.parse(jsonmem, root)){
-        int mem = root[id][0]["memusage"].asInt();
-        root[id][0]["memusage"] = 0;
-        int userid = stoi(id.substr(0, 1));
-        root["loginuser"][userid][current_user] = root["loginuser"][userid][current_user].asInt() - mem;
         root.removeMember(id);
-    }
+        int mem = root["taskmem"][id].asInt();
+        root["taskmem"][id] = 0;
+        root["user"][current_user] = root["user"][current_user].asInt() - mem;
+    }   
     jsonmem = root.toStyledString();
 }
 
-bool limit_check(vector<string> user_name, int mem){
+bool limit_check(int mem){
     Json::Reader reader;
     Json::Value root;
 
     int usage = 0;
     if (reader.parse(jsonmem, root)){
-        for (int i = 0; i<user_name.size(); i++){
-            usage += root["loginuser"][i][user_name[i]].asInt();
+        for (int i = 0; i < root["user"].size(); i++){
+            usage += root["user"][user_name[i]].asInt();
         }
     }
     if ((usage + mem) >= mem_capacity){
@@ -159,5 +134,22 @@ bool limit_check(vector<string> user_name, int mem){
     }else return true;
 }
 
+int get_task_mem(int taskid){
+    Json::Reader reader;
+    Json::Value root;
+    // cout << jsonmem;
 
+    if (reader.parse(jsonmem, root)){
+        return root["taskmem"][taskid].asInt();
+    }
+}
 
+int get_user_mem(string username){
+    Json::Reader reader;
+    Json::Value root;
+    // cout << jsonmem;
+
+    if (reader.parse(jsonmem, root)){
+        return root["user"][username].asInt();
+    }
+}
