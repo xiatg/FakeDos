48d5da391ac3a70ac1c103a93a2943a5bbe79544
J3pHR3-Y <47342498+J3pHR3-Y@users.noreply.github.com>
2020-05-29 00:40:17 +0800

    delete taskid storage
    use taskid stored in taskmanege

--- mem.h
+++ mem.h
@@ -13,23 +13,27 @@
 
 
 
+
 void init_mem();
 
-void user_mem_alloc(std::string & current_user);
+void user_mem_alloc(std::string username);
 
 void task_mem(int taskid, int taskmem);
 
 template <typename ValueType>
-bool task_data_write(int taskid, int taskmem);
+bool task_data_write(int taskid, std::string key, ValueType value);
 
 template <typename ValueType>
 ValueType task_data_read(int taskid, std::string key);
 
-
-void user_mem_free(std::string & current_user);
+void user_mem_free(std::string username);
 
 void task_mem_free(int taskid);
 
-bool limit_check(std::vector<std::string> user_name, int mem);
+bool limit_check(std::vector<string> user_name, int mem);
+
+int get_task_mem(int taskid);
+
+int get_user_mem(std::string username)
 
 #endif 
