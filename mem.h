18e76371d910030fa62d608484a70c2720c8538a
J3pHR3-Y <47342498+J3pHR3-Y@users.noreply.github.com>
2020-05-29 17:03:33 +0800

    Update mem.h

--- mem.h
+++ mem.h
@@ -13,27 +13,27 @@
 
 
 
-
 void init_mem();
 
-void user_mem_alloc(std::string username);
+void user_mem_alloc(std::string username, std::string jsonmem);
 
-void task_mem(int taskid, int taskmem);
-
-template <typename ValueType>
-bool task_data_write(int taskid, std::string key, ValueType value);
+void task_mem(int taskid, int taskmem, std::string username, std::string jsonmem);
 
 template <typename ValueType>
-ValueType task_data_read(int taskid, std::string key);
+bool task_data_write(int taskid, std::string key, ValueType value, std::string jsonmem);
 
-void user_mem_free(std::string username);
+template <typename ValueType>
+ValueType task_data_read(int taskid, std::string key, std::string jsonmem);
 
-void task_mem_free(int taskid);
+void user_mem_free(std::string username, std::string jsonmem);
 
-bool limit_check(std::vector<std::string> user_name, int mem);
+void task_mem_free(int taskid, std::string jsonmem);
 
-int get_task_mem(int taskid);
+bool limit_check(std::vector<string> user_name, int mem, std::string jsonmem);
 
-int get_user_mem(std::string username);
+int get_task_mem(int taskid, std::string jsonmem);
+
+int get_user_mem(std::string username, std::string jsonmem);
+
 
 #endif
