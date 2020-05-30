cc358de53fef10de56adfc380a6b613cb4abece9
J3pHR3-Y <47342498+J3pHR3-Y@users.noreply.github.com>
2020-05-30 14:10:10 +0800

    Update mem.h

--- mem.h
+++ mem.h
@@ -1,8 +1,10 @@
-/*
- * File: mem.h
- * -------------
- * This interface 
- */
+
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#elif defined(_MSC_VER)
+#pragma warning(disable : 4996)
+#endif
 
 #ifndef MEM_H
 #define MEM_H
@@ -20,7 +22,7 @@ void user_mem_alloc(std::string username, std::string jsonmem);
 void task_mem(int taskid, int taskmem, std::string username, std::string jsonmem);
 
 template <typename ValueType>
-bool task_data_write(int taskid, std::string key, ValueType value, std::vector<std::string> user_name, std::string jsonmem);
+bool task_data_write(int taskid, std::string key, ValueType value, std::vector<std::string> user_name, std::string username, std::string jsonmem);
 
 template <typename ValueType>
 ValueType task_data_read(int taskid, std::string key, std::string jsonmem);
@@ -29,7 +31,7 @@ void user_mem_free(std::string username, std::string jsonmem);
 
 void task_mem_free(int taskid, std::string jsonmem);
 
-bool limit_check(std::vector<std::string> user_name, int mem, std::string jsonmem);
+bool limit_check(int mem, std::vector<std::string> user_name, std::string jsonmem);
 
 int get_task_mem(int taskid, std::string jsonmem);
 
