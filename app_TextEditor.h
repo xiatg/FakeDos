65ff8ccbb8084b8a956f63a1e290954d53533af8
J3pHR3-Y <47342498+J3pHR3-Y@users.noreply.github.com>
2020-05-30 23:58:29 +0800

    add hangon and wakeup feature

--- app_TextEditor.h
+++ app_TextEditor.h
@@ -2,6 +2,7 @@
 #define APP_TEXTEDITOR_H
 
 #include <string>
+#include <vector>
 
 /* Editor Buffer: implements a simple text editor.
  * The underlying data structure is a linked list.
@@ -31,6 +32,9 @@ public:
     // Show the contents of the editor, as well as the location of cursor
     void showContents();
 
+    // Hang on editor by store text data in memory and then close the editor
+    bool HangOnEditor(int taskid, std::vector <std::string> user_name, std::string username, std::string & jsonmem);
+    
 private:
     /* Cell: the basic unit of linked-list-type editor buffer.
      * Each cell contains a character and a pointer to the next cell.
@@ -53,6 +57,8 @@ void editText(EditorBuffer &editor);
 // Print a command list for editor operations
 void editorHelp();
 
+// Initialize editor with data store in memory if it is a wake-up task
+void TextEditor(int taskid, bool wakeup, std::string & jsonmem, int textsize);
 
 #endif // APP_TEXTEDITOR_H
 
