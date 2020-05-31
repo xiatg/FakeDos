4bf77c89f88cf5ac4065b775f358b99f28ad7428
slowhite <62301447+slowhite@users.noreply.github.com>
2020-05-31 20:56:43 +0800

    Add files via upload

--- TODOlist.cpp
+++ TODOlist.cpp
@@ -36,14 +36,29 @@ void TODO_List(int taskid,
    /*retrieve data*/
    if (!first_exe)
    {
+       // get the number of events
+       int num;
+       num = stoi(task_data_read(taskid, "number", jsonmem));
+
       // retrieve the data with the given taskid
-       for(int i=0;i<list_for_all.size();i++){
-      list_for_all[i].name = task_data_read(taskid, "name"+to_string(i), jsonmem);
-      list_for_all[i].remark = task_data_read(taskid, "remark"+to_string(i), jsonmem);
-      list_for_all[i].state = stoi(task_data_read(taskid, "state"+to_string(i), jsonmem));
-
-       }
-
+      for (int i = 0; i < num; i++)
+      {
+         string name = task_data_read(taskid, "name" + to_string(i), jsonmem);
+         string remark = task_data_read(taskid, "remark" + to_string(i), jsonmem);
+         int state = stoi(task_data_read(taskid, "state" + to_string(i), jsonmem));
+         thing.name =name;
+         thing.remark = remark;
+         thing.state = state;
+         list_for_all.push_back(thing);
+      }
+      cout << "Welcome BACK to your todo list!" << endl;
+      cout << "You can add events to your todo list and tick when you done it." << endl;
+      cout << "You can see your todo list at any time!" << endl;
+      cout << "Enter 'add' to add the event,enter 'tick' to tick your event, "
+              "enter 'display' to look your todo list."
+           << endl;
+      cout << "Type 'hangon' to save your changes, you can later open it." << endl;
+      cout << "Type 'exit' to exit the todo list." << endl;
    }
    else
    {
@@ -56,22 +71,24 @@ void TODO_List(int taskid,
       cout << "Type 'hangon' to save your changes, you can later open it." << endl;
       cout << "Type 'exit' to exit the todo list." << endl;
       bool flag = true;
-      for(int i=0;i<list_for_all.size();i++)
+      for (int i = 0; i < list_for_all.size(); i++)
       {
-          bool f0 = task_data_write(taskid, "name"+to_string(i), list_for_all[i].name, user_name, username, jsonmem);
-          bool f1 = task_data_write(taskid, "remark"+to_string(i), list_for_all[i].remark, user_name, username, jsonmem);
-          bool f2 = task_data_write(taskid, "state"+to_string(i),to_string(list_for_all[i].state),user_name,username,jsonmem);
-          if(f0||f1||f2==false){
-              flag = false;
-          }
-      }
+         bool f0 = task_data_write(taskid, "name" + to_string(i), list_for_all[i].name, user_name, username, jsonmem);
+         bool f1 = task_data_write(taskid, "remark" + to_string(i), list_for_all[i].remark, user_name, username, jsonmem);
+         bool f2 = task_data_write(taskid, "state" + to_string(i), to_string(list_for_all[i].state), user_name, username, jsonmem);
 
-        if(flag==false)
-        {
+         if ((f0 == false) || (f1 == false) || (f2 == false))
+         {
+            flag = false;
+            break;
+         }
+      }
+      if (flag == false)
+      {
          cout << "Error: memory overflow. App is terminating." << endl;
          kill(username, taskid, jsonmem, mem, runningQueue, blockQueue, readyQueue);
          return;
-         }
+      }
    }
 
    /*main process*/
@@ -99,7 +116,7 @@ void TODO_List(int taskid,
                getline(cin, thing.remark);
                thing.state = 0;
                list_for_all.push_back(thing);
-               cout<<"Successfully created!"<<endl;
+               cout << "Successfully created!" << endl;
             }
             else if (command == "tick")
             {
@@ -112,32 +129,39 @@ void TODO_List(int taskid,
                   if (iter->name == s)
                      iter->state = 1;
                }
-               cout<<s<<"is ticked."<<endl;
+               cout << s << "is ticked." << endl;
             }
             else if (command == "display")
             {
-               vector<event>::iterator iter;
-               int find = 0;
-               for (iter = list_for_all.begin(); iter != list_for_all.end(); iter++)
+               if (list_for_all.empty())
                {
-                  if (iter->state == 1)
-                  {
-                     cout << iter->name << " is done." << endl;
-                  }
-                  else if (iter->state == 0)
-                  {
-                     cout << iter->name << " is undone." << endl;
-                     cout << "Remark:" << iter->remark << endl;
-                     find++;
-                  }
-               }
-               if (find == 0)
-               {
-                  cout << "Congratulations! You have done all the things!" << endl;
+                  cout << "Empty list!" << endl;
                }
                else
                {
-                  cout << "You still have " << find << " thing(s) to do." << endl;
+                  vector<event>::iterator iter;
+                  int find = 0;
+                  for (iter = list_for_all.begin(); iter != list_for_all.end(); iter++)
+                  {
+                     if (iter->state == 1)
+                     {
+                        cout << iter->name << " is done." << endl;
+                     }
+                     else if (iter->state == 0)
+                     {
+                        cout << iter->name << " is undone." << endl;
+                        cout << "Remark:" << iter->remark << endl;
+                        find++;
+                     }
+                  }
+                  if (find == 0)
+                  {
+                     cout << "Congratulations! You have done all the things!" << endl;
+                  }
+                  else
+                  {
+                     cout << "You still have " << find << " thing(s) to do." << endl;
+                  }
                }
             }
          }
@@ -152,25 +176,29 @@ void TODO_List(int taskid,
    else
    {
       // save the useful data to memory with the given taskid
-       bool flag = true;
-       for(int i=0;i<list_for_all.size();i++)
-       {
-           bool f0 = task_data_write(taskid, "name"+to_string(i), list_for_all[i].name, user_name, username, jsonmem);
-           bool f1 = task_data_write(taskid, "remark"+to_string(i), list_for_all[i].remark, user_name, username, jsonmem);
-           bool f2 = task_data_write(taskid, "state"+to_string(i),to_string(list_for_all[i].state),user_name,username,jsonmem);
+      bool flag = true;
+      for (int i = 0; i < list_for_all.size(); i++)
+      {
+         bool f0 = task_data_write(taskid, "name" + to_string(i), list_for_all[i].name, user_name, username, jsonmem);
+         bool f1 = task_data_write(taskid, "remark" + to_string(i), list_for_all[i].remark, user_name, username, jsonmem);
+         bool f2 = task_data_write(taskid, "state" + to_string(i), to_string(list_for_all[i].state), user_name, username, jsonmem);
+         bool f3 = task_data_write(taskid, "number",to_string(list_for_all.size()),user_name, username, jsonmem);
 
-           if((f0 == false)||(f1 == false)||(f2==false)){
-               flag = false;
-               break;
-           }
-       }
+         if ((f0 == false) || (f1 == false) || (f2 == false)||(f3 == false))
+         {
+            flag = false;
+            break;
+         }
+      }
 
-      if (flag==false)
+      if (flag == false)
       {
          cout << "Error: memory overflow. App is terminating." << endl;
          kill(username, taskid, jsonmem, mem, runningQueue, blockQueue, readyQueue);
-      } else {
-          block(username, taskid, mem, runningQueue, blockQueue, readyQueue);
+      }
+      else
+      {
+         block(username, taskid, mem, runningQueue, blockQueue, readyQueue);
       }
    }
 }
